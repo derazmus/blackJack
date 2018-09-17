891cb6279186ee33a0184f419e3947169cbcb1c1
Denise Erazmus <denise.erazmus@gmail.com>
2018-09-17 14:43:11 -0500

    inital set up

---
+++ app.js
@@ -0,0 +1,12 @@
+//Blackjack 
+//by Denise Erazmus
+
+let card1 = "Ace of Spades",
+    card2 = "Ten of hearts";
+
+
+console.log("Welcome to Blackjack");
+
+console.log("You are dealt: ");
+console.log("  " + card1);
+console.log("  " + card2);
