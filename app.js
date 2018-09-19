49cf0d2931606909a5cce1e7da8bd291fce7673c
Denise Erazmus <denise.erazmus@gmail.com>
2018-09-19 14:08:02 -0500

    loops to create decks

--- app.js
+++ app.js
@@ -3,15 +3,28 @@
 
 
 //card array
-let deck = [
-	"Ace of Spades",
-	"Two of Spades",
-	"Three of Spades"
+
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight',
+    'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'
 ];
+let deck = [];
+
+//loops to create deck of 52 cards
+for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+        deck.push(values[valueIdx] + '  of  ' + suits[suitIdx]);
+    }
+}
+
+for(let i = 0; i < deck.length; i++){
+	console.log(deck[i]);
+}
 
 //array that holds player cards
 
-let playerCards = [ deck[0], deck[2] ];
+let playerCards = [deck[0], deck[2]];
 
 console.log("Welcome to Blackjack");
 
